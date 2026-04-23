d3f3f19ba5c336480026852c2505052e3a77835e
jeremyfidelio <tipawaeljohn@gmail.com>
2026-04-23 12:55:44 +0700

    71241118

---
+++ Pemrosesan Hasil Pertandingan-20260423T025931Z-3-001/Pemrosesan Hasil Pertandingan/file.py
@@ -0,0 +1,3 @@
+def parse_winner(filename):
+    filename = ["C:\Users\Admin\Downloads\ug-bfilemanagement-John867250\Pemrosesan Hasil Pertandingan-20260423T025931Z-3-001\Pemrosesan Hasil Pertandingan\LaLiga.txt", "C:\Users\Admin\Downloads\ug-bfilemanagement-John867250\Pemrosesan Hasil Pertandingan-20260423T025931Z-3-001\Pemrosesan Hasil Pertandingan\UCL.txt" , "C:\Users\Admin\Downloads\ug-bfilemanagement-John867250\Pemrosesan Hasil Pertandingan-20260423T025931Z-3-001\Pemrosesan Hasil Pertandingan\PremierLeague.txt"]
+    open(filename)
